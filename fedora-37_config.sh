2e548ab34f887a9c3782b5ee3749229b91d0ce9e
CodeWracker <dev.rodrigofs@gmail.com>
2023-01-24 13:06:05 -0300

    adicionei mais alguns programas

--- fedora-37_config.sh
+++ fedora-37_config.sh
@@ -5,13 +5,13 @@ sudo dnf update -y
 
 # Função para selecionar/desmarcar opções
 choice () {
-	local choice=$1
-	if [[ ${opts[choice]} ]] # toggle
-	then
-		opts[choice]=
-	else
-		opts[choice]=+
-	fi
+    local choice=$1
+    if [[ ${opts[choice]} ]] # toggle
+    then
+        opts[choice]=
+    else
+        opts[choice]=+
+    fi
 }
 verify_option_all (){
     if [[ ${opts[1]} ]]
@@ -24,43 +24,59 @@ verify_option_all (){
 while :
 do
     clear
-    options=("Pronto" "Seleciona Tudo ${opts[1]}" "Instalar Onedriver ${opts[2]}" "Instalar Steam ${opts[3]}" "Instalar VSCode ${opts[4]}" "Configurar alias: cls -> clear ${opts[5]}")
+    options=("Pronto" "Seleciona Tudo ${opts[1]}" "Instalar Onedriver ${opts[2]}" "Instalar Steam ${opts[3]}" "Instalar VSCode ${opts[4]}" "Configurar alias: cls -> clear ${opts[5]}" "Instalar python 3.9 ${opts[6]}" "Instalar Microsoft Edge ${opts[7]}" "Instalar o PIP ${opts[8]}" "Instalar unrar e unzip ${opts[9]}")
     printf 'escolha o que deve ser feito:%s\n'
-    for i in {0..5}
+    for i in {0..9}
     do
-    	echo "${i}) ${options[i]}"
-	done
+        echo "${i}) ${options[i]}"
+    done
     read -p "Selcionado: " opt
-	case $opt in
-		0)
-			break
-			;;
-		1)
-			# Seleciona/desmarca todas as opções
-			for i in {2..5}
-			do
-				opts[i]=+
-			done
-			choice 1
-			;;
-		2)
-			verify_option_all
-			choice 2
-			;;
-		3)
-			verify_option_all
-			choice 3
-			;;
-		4)
-			verify_option_all
-			choice 4
-			;;
-		5)
-			verify_option_all
-			choice 5
-			;;
-		*) printf '%s\n' 'Opção inválida';;
-	esac
+    case $opt in
+        0)
+            break
+        ;;
+        1)
+            # Seleciona/desmarca todas as opções
+            for i in {2..5}
+            do
+                opts[i]=+
+            done
+            choice 1
+        ;;
+        2)
+            verify_option_all
+            choice 2
+        ;;
+        3)
+            verify_option_all
+            choice 3
+        ;;
+        4)
+            verify_option_all
+            choice 4
+        ;;
+        5)
+            verify_option_all
+            choice 5
+        ;;
+        6)
+            verify_option_all
+            choice 6
+        ;;
+        7)
+            verify_option_all
+            choice 7
+        ;;
+        8)
+            verify_option_all
+            choice 8
+        ;;
+        9)
+            verify_option_all
+            choice 9
+        ;;
+        *) printf '%s\n' 'Opção inválida';;
+    esac
     
 done
 
@@ -82,36 +98,69 @@ done
 # Instala o Onedriver
 if [[ ${opts[2]} ]]
 then
-	sudo dnf copr enable jstaf/onedriver -y
-	sudo dnf install onedriver -y
-	clear
+    sudo dnf copr enable jstaf/onedriver -y
+    sudo dnf install onedriver -y
+    clear
 fi
 
 # Instala o Steam
 if [[ ${opts[3]} ]]
 then
-	sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo -y
-	sudo dnf install steam -y
-	clear
+    sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo -y
+    sudo dnf install steam -y
+    clear
 fi
 
 # Instala o VSCode
 if [[ ${opts[4]} ]]
 then
-	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
-	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' -y
-	sudo dnf install code -y
-	clear
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc -y
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo' -y
+    sudo dnf install code -y
+    clear
 fi
 
 # Cria um alias para o comando clear
 if [[ ${opts[5]} ]]
 then
-	echo "alias cls='clear'" >> ~/.bashrc
-	source ~/.bashrc
-	echo "alias cls='clear'" >> ~/.zshrc
-	source ~/.zshrc
-	clear
+    echo "alias cls='clear'" >> ~/.bashrc
+    source ~/.bashrc
+    echo "alias cls='clear'" >> ~/.zshrc
+    source ~/.zshrc
+    clear
+fi
+
+
+# Instala o python 3.9
+if [[ ${opts[6]} ]]
+then
+    sudo dnf install python39 -y
+    clear
+fi
+
+# Instala o Microsoft Edge
+if [[ ${opts[7]} ]]
+then
+    # Adiciona o repositório do Microsoft Edge
+    sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge -y
+    
+    # Instala o Microsoft Edge
+    sudo dnf install microsoft-edge-dev -y
+    clear
+fi
+
+# Instala o PIP
+if [[ ${opts[8]} ]]
+then
+    sudo dnf install python3-pip -y
+    clear
+fi
+
+# Instala o unrar e unzip
+if [[ ${opts[9]} ]]
+then
+    sudo dnf install unrar unzip -y
+    clear
 fi
 
 # instruções para mais configurações
@@ -119,23 +168,50 @@ fi
 # Onedriver
 if [[ ${opts[2]} ]]
 then
-	printf "Abra o app do onedriver e escolha a pasta que quer sincronizar e faça o login %s\n"
+    printf "Abra o app do onedriver e escolha a pasta que quer sincronizar e faça o login %s\n"
 fi
 
 # Steam
 if [[ ${opts[3]} ]]
 then
-	printf "Abra a steam e selecione o proton nas configurações do steam play %s\n"
+    printf "Abra a steam e selecione o proton nas configurações do steam play %s\n"
 fi
 
-# VSCode
-#if [[ ${opts[4]} ]]
-#then
-#
-#fi
+#VSCode
+if [[ ${opts[4]} ]]
+then
+    printf "Abra o VSCode e logue na sua conta %s\n"
+fi
 
 # lias para o comando clear
 if [[ ${opts[5]} ]]
 then
-	printf "Para o alias cls surtir efeito reinicie o terminal %s\n"
+    printf "Para o alias cls surtir efeito reinicie o terminal %s\n"
+fi
+
+# python 3.9
+if [[ ${opts[6]} ]]
+then
+    printf "Para ver a versão do python 3.9 digite: python3.9 --version %s\n"
+    python3.9 --version
+fi
+
+# Microsoft Edge
+if [[ ${opts[7]} ]]
+then
+    printf "Abra o Microsoft Edge e faça o login %s\n"
+fi
+
+# PIP
+if [[ ${opts[8]} ]]
+then
+    printf "Para ver a versão do pip digite: pip --version %s\n"
+    pip --version
+fi
+
+# unrar e unzip
+if [[ ${opts[9]} ]]
+then
+    printf "Para descompactar um arquivo rar digite: unrar e <nome do arquivo> %s\n"
+    printf "Para descompactar um arquivo zip digite: unzip <nome do arquivo> %s\n"
 fi
